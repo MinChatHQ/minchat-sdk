72a2a6911e377aeff6575f153c3ed8b2af479737
Mundia <fxmundia416@gmail.com>
2025-06-21 14:30:37 +0200

    bug fix

--- packages/js/src/example/main.ts
+++ packages/js/src/example/main.ts
@@ -1,7 +1,7 @@
 import { App } from './App'
 
 const testApiKey = 'CMBPFNMXW0000WEV26EYE6TUT'
-const prodAPiKey = 'CLQ8QXTRG00003MKP13FNB1TY'
+const prodAPiKey = ''
 const apiKey = testApiKey || prodAPiKey 
 
 // Ensure there is a root element
